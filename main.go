97be2d80b098c8f6249ef1f08725620e8f7b3503
luckcha <radheyy1805@gmail.com>
2025-01-31 17:56:31 +0530

    code update

--- main.go
+++ main.go
@@ -1,21 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, Lucky!")
+type Book struct {
+	Book_Name   string
+	Publication string
+	Chapter     int
+	Price       float64
+}
+
+var Product []Book
+
+func homepage(w http.ResponseWriter, r *http.Request) {
+	log.Println("Endpont hit:homepage")
+	fmt.Fprintf(w, "Hey there lucky")
+}
+
+func returnAllProduct(w http.ResponseWriter, r *http.Request) {
+	log.Println("Endpoint hit: returnAllProduct")
+	json.NewEncoder(w).Encode(Product)
+}
+
+func handleRequests() {
+	http.HandleFunc("/Product", returnAllProduct)
+	http.HandleFunc("/", homepage)
+	http.ListenAndServe("localhost:10000", nil)
+
 }
 
 func main() {
-
-	http.HandleFunc("/", handler)
-
-	fmt.Println("Starting the server on port 10000...")
-	if err := http.ListenAndServe(":10000", nil); err != nil {
-		log.Fatal(err)
+	Product = []Book{
+		{Book_Name: "Science", Publication: "Mittal", Chapter: 15, Price: 390.00},
+		{Book_Name: "Mathematics", Publication: "Oxford", Chapter: 12, Price: 450.00},
+		{Book_Name: "History", Publication: "Penguin", Chapter: 20, Price: 550.00},
+		{Book_Name: "Hindi", Publication: "Mittal", Chapter: 10, Price: 350.00},
+		{Book_Name: "Economy", Publication: "Oxford", Chapter: 17, Price: 300.00},
 	}
+
+	handleRequests()
+
 }
